3b87909878f591f3a20d29a72f8d21612a28531b
Alexandru Corut <r3dm4n@me.com>
2017-05-11 18:09:31 +0300

    Saving Pokemon to a GPS location

--- Poke Finder/ViewController.swift
+++ Poke Finder/ViewController.swift
@@ -8,24 +8,29 @@
 
 import UIKit
 import MapKit
+import FirebaseDatabase
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    
+
     let locationManager = CLLocationManager()
     var mapHasCenteredOnce = false
-    
+
+    var geoFire: GeoFire!
+    var geoFireRef: FIRDatabaseReference!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        
-        
+
         mapView.delegate = self
         mapView.userTrackingMode = MKUserTrackingMode.follow
-//        var geoFire: GeoFire!
+
+        geoFireRef = FIRDatabase.database().reference()
+        geoFire = GeoFire(firebaseRef: geoFireRef)
+
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         locationAuthStatus()
     }
@@ -37,19 +42,19 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             locationManager.requestWhenInUseAuthorization()
         }
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == CLAuthorizationStatus.authorizedWhenInUse {
             mapView.showsUserLocation = true
         }
     }
-    
+
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
-        
+
         mapView.setRegion(coordinateRegion, animated: true)
     }
-    
+
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         if let loc = userLocation.location {
             if !mapHasCenteredOnce {
@@ -58,23 +63,48 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             }
         }
     }
-    
-    
+
+
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
+
         var annotationView: MKAnnotationView?
-        
-        
+
+
         if annotation.isKind(of: MKUserLocation.self) {
             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
             annotationView?.image = UIImage(named: "ash")
         }
-        
+
         return annotationView
     }
-    
+
+    func createSighting(forLocation location: CLLocation, withPokemon pokeId: Int) {
+
+        geoFire.setLocation(location, forKey: "\(pokeId)")
+    }
+
+    func showSightingsOnMap(location: CLLocation) {
+        let circleQuery = geoFire!.query(at: location, withRadius: 2.5)
+
+        _ = circleQuery?.observe(GFEventType.keyEntered, with: { (key, location) in
+
+            if let key = key, let location = location {
+                let anno = PokeAnnotation(coordinate: location.coordinate, pokemonNumber: Int(key)!)
+                self.mapView.addAnnotation(anno)
+                
+            }
+
+        })
+    }
 
     @IBAction func spotRandomPokemon(_ sender: Any) {
+        
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        
+        let rand = arc4random_uniform(151) + 1
+        
+        createSighting(forLocation: loc, withPokemon: Int(rand))
+        
     }
 
 }
