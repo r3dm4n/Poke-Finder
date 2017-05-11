f068fda7d4f4861d749409b6a4f4ed759f850b2d
Alexandru Corut <r3dm4n@me.com>
2017-05-11 19:06:27 +0300

    Location permission

--- Poke Finder/ViewController.swift
+++ Poke Finder/ViewController.swift
@@ -126,7 +126,15 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
 
         if let anno = view.annotation as? PokeAnnotation {
-            let place = MKPlacemark(coordinate: anno.coordinate)
+            
+            
+            var place: MKPlacemark!
+            if #available(iOS 10.0, *) {
+                place = MKPlacemark(coordinate: anno.coordinate)
+            } else {
+                place = MKPlacemark(coordinate: anno.coordinate, addressDictionary: nil)
+            }
+            
             let destination = MKMapItem(placemark: place)
             destination.name = "Pokemon Sighting"
             let regionDistance: CLLocationDistance = 1000
